4ca6333e975d26e6ce2aa4acd3f97f8fd79b5e30
toxichnayatvar <123656439+toxichnayatvar@users.noreply.github.com>
2023-06-27 00:48:36 +0300

    Update app.js

--- app.js
+++ app.js
@@ -73,6 +73,66 @@ function wtf() {
             res += '?'
         } else if (text[i] === '?') {
             res += ','
+        } else if (text[i] === 'ፍ') {
+            res += 'а'
+        } else if (text[i] === '፣') {
+            res += 'б'
+        } else if (text[i] === 'ድ') {
+            res += 'в'
+        } else if (text[i] === 'ኡ') {
+             res += 'г'
+        } else if (text[i] === 'ል') {
+             res += 'д'
+        } else if (text[i] === 'ት') {
+             res += 'е'
+        } else if (text[i] === '፤') {
+             res += 'ж'
+        } else if (text[i] === 'ፕ') {
+             res += 'з'
+        } else if (text[i] === 'ብ') {
+            res += 'и'
+        } else if (text[i] === 'ቅ') {
+             res += 'й'
+        } else if (text[i] === 'ር') {
+             res += 'к'
+        } else if (text[i] === 'ክ') {
+             res += 'л'
+        } else if (text[i] === 'ቭ') {
+            res += 'м'
+        } else if (text[i] === 'ይ') {
+            res += 'н'
+        } else if (text[i] === 'ጅ') {
+             res += 'о'
+        } else if (text[i] === 'ግ') {
+            res += 'п'
+        } else if (text[i] === 'ህ') {
+            res += 'р'
+        } else if (text[i] === 'ች') {
+            res += 'с'
+        } else if (text[i] === 'ን') {
+            res += 'т'
+        } else if (text[i] === 'እ') {
+            res += 'у'
+        } else if (text[i] === 'አ') {
+            res += 'ф'
+        } else if (text[i] === '[') {
+           res += 'х'
+        } else if (text[i] === 'ው') {
+            res += 'ц'
+        } else if (text[i] === 'ሽ') {
+            res += 'ч'
+        } else if (text[i] === 'ኢ') {
+            res += 'ш'
+        } else if (text[i] === 'ኦ') {
+            res += 'щ'
+        } else if (text[i] === 'ስ') {
+            res += 'ы'
+        } else if (text[i] === 'ም') {
+            res += 'ъ'
+        } else if (text[i] === '።') {
+            res += 'ю'
+        } else if (text[i] === 'ዝ') {
+            res += 'я'
         } else {
             res += text[i];
         }
